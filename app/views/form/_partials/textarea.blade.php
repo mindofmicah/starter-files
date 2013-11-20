1cb97793016ff80cfd6cc0b25e6dbee5c34107e2
Micah <micah@whamdonk.com>
2013-11-20 09:08:23 -0600

    Created partials for quickly printing form elements

---
+++ app/views/form/_partials/textarea.blade.php
@@ -0,0 +1,5 @@
+<div class="form-group {{($errors->first($field) ? ' has-error' : '')}}">
+    {{Form::label($field, isset($label)?$label:ucwords($field), array('class'=>'control-label'))}}
+    <div class="pull-right help-block">{{$errors->first($field)}}</div>
+    {{Form::textarea($field, null, array('class'=>'form-control'))}}
+</div>
