e898be7ff04d6787b177ab5dbe39ff748eeb0b0e
Micah <micah@whamdonk.com>
2013-11-29 12:11:23 -0600

    Add the admin navigation to the main route file

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,8 @@
 <?php
+View::composer('admin.nav', function($view) {
+    $admin_links = AdminLink::orderBy('order')->get(array('href', 'label'));
+    $view->with('links', $admin_links);
+});
 Route::get('/', function () {
     return View::make('home');
 });
