593d7088b3589ce32b2c08ba96c961f45a2954da
Micah Eschbacher <michaeleschbacher@gmail.com>
2014-01-20 12:38:01 -0600

    Added functionality for various form elements

--- app/views/form/_partials/text.blade.php
+++ app/views/form/_partials/text.blade.php
@@ -1,5 +1,11 @@
 <div class="form-group {{($errors->first($field) ? ' has-error' : '')}}">
     {{Form::label($field, isset($label)?$label:ucwords($field), array('class'=>'control-label'))}}
     <div class="pull-right help-block">{{$errors->first($field)}}</div>
-    {{Form::text($field, null, array('class'=>'form-control'))}}
+    @if(empty($type))
+        {{Form::text($field, null, array('class'=>'form-control'))}}
+    @elseif($type == 'password')
+        {{Form::password($field, array('class'=>'form-control'))}}
+    @else
+        {{Form::$type($field, null, array('class'=>'form-control'))}}
+    @endif
 </div>
